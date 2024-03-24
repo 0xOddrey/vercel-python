f959b6b701eda585c3296d43413723488a541259
0xOddrey <97917541+0xOddrey@users.noreply.github.com>
2024-03-23 20:16:56 -0500

    updates

--- api/index.py
+++ api/index.py
@@ -1,19 +1,29 @@
 from http.server import BaseHTTPRequestHandler
 from urllib import parse
+import spacy
+
+nlp = spacy.load("en_core_web_sm")
+
+def convert_decimal_to_score(decimal):
+    if decimal < 0 or decimal > 1:
+        return 1499
+    
+    return 1 + (1 - decimal) * (1500 - 1)
+
 
 class handler(BaseHTTPRequestHandler):
 
 	def do_GET(self):
 		s = self.path
 		dic = dict(parse.parse_qsl(parse.urlsplit(s).query))
+		word = dic["word"]
+		word = word.lower()
+		sim = nlp('shark').similarity(nlp(word))
+		score = convert_decimal_to_score(sim)
+
 		self.send_response(200)
 		self.send_header('Content-type','text/plain')
 		self.end_headers()
-
-		if "name" in dic:
-			message = "Hello, " + dic["name"] + "!"
-		else:
-			message = "Hello, stranger!"
-
+		message = str(score)
 		self.wfile.write(message.encode())
 		return
