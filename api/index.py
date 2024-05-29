8603234c8221055c17b7f1c6357f856b4ccb2ef1
Audrey Akwenye <130921094+audrey-spartan@users.noreply.github.com>
2024-05-28 19:49:34 -0500

    updates

--- api/index.py
+++ api/index.py
@@ -1,11 +1,10 @@
 from http.server import BaseHTTPRequestHandler
 from urllib import parse
+import spacy
 import json
-from sklearn.metrics.pairwise import cosine_similarity
-import gensim.downloader as api
 
-# Download and load the pre-trained Word2Vec model
-model = api.load("glove-twitter-25")
+
+nlp = spacy.load("en_core_web_md")
 
 
 
@@ -29,15 +28,7 @@ class handler(BaseHTTPRequestHandler):
 		dic = dict(parse.parse_qsl(parse.urlsplit(s).query))
 		word = dic["word"]
 		answer = dic['answer']
-		# Get the vector representation of another word (e.g., "cheese")
-		word_vector = model[word]
-		answer_vector = model[answer]
-
-		# Reshape vectors for cosine similarity calculation
-		word_vector = word_vector.reshape(1, -1)  # Reshape to row vector
-		answer_vector = answer_vector.reshape(1, -1)    # Reshape to row vector
-		sim = cosine_similarity(word_vector, answer_vector)[0][0]
-		# Calculate cosine similarity between "pizza" and the other word
+		sim = nlp(answer).similarity(nlp(word))
 		score =  sim * 100
 		result = json.dumps({"score": score})
 		self.send_response(200)
