756d7be9d1f2ecc46da23533163aba6c5404549f
0xOddrey <97917541+0xOddrey@users.noreply.github.com>
2024-06-01 11:48:11 -0500

    updates

--- api/words/index.py
+++ api/words/index.py
@@ -3,13 +3,7 @@ from urllib import parse
 import json
 import gensim.downloader as api
 import numpy as np
-model = api.load('glove-twitter-25')
 
-def cosine_similarity(vec1, vec2):
-    dot_product = np.dot(vec1, vec2)
-    norm_vec1 = np.linalg.norm(vec1)
-    norm_vec2 = np.linalg.norm(vec2)
-    return dot_product / (norm_vec1 * norm_vec2)
 
 
 class handler(BaseHTTPRequestHandler):
