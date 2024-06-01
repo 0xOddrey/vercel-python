647438e0e9fe5207517ae7d8ae0c075ec58f74d7
0xOddrey <97917541+0xOddrey@users.noreply.github.com>
2024-06-01 10:41:36 -0500

    updates

--- api/index.py
+++ api/index.py
@@ -1,8 +1,16 @@
 from http.server import BaseHTTPRequestHandler
 from urllib import parse
-
+import numpy as np
 import json
+import gensim.downloader as api
+# Download and load the pre-trained Word2Vec model
+model = api.load("glove-twitter-25")
 
+def cosine_similarity(vec1, vec2):
+    dot_product = np.dot(vec1, vec2)
+    norm_vec1 = np.linalg.norm(vec1)
+    norm_vec2 = np.linalg.norm(vec2)
+    return dot_product / (norm_vec1 * norm_vec2)
 
 class handler(BaseHTTPRequestHandler):
 
@@ -24,7 +32,11 @@ class handler(BaseHTTPRequestHandler):
 		dic = dict(parse.parse_qsl(parse.urlsplit(s).query))
 		word = dic["word"]
 		answer = dic['answer']
-		result = json.dumps({"score": 5})
+		answer_vector = model[answer]
+		word_vector = model[word]
+		sim = cosine_similarity(answer_vector, word_vector)
+		score = sim * 100
+		result = json.dumps({"score": score})
 		self.send_response(200)
 		self._set_headers()
 		self.end_headers()
