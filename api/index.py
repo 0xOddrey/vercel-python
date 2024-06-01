97950753a0008b2e84b5766b57e557c21c71f2e8
0xOddrey <97917541+0xOddrey@users.noreply.github.com>
2024-06-01 10:24:43 -0500

    updates

--- api/index.py
+++ api/index.py
@@ -1,9 +1,10 @@
 from http.server import BaseHTTPRequestHandler
 from urllib import parse
+import spacy
 import json
-import gensim.downloader as api
-from sklearn.metrics.pairwise import cosine_similarity
-model = api.load("glove-twitter-25")
+
+nlp = spacy.load("en_core_web_md")
+
 
 class handler(BaseHTTPRequestHandler):
 
@@ -25,16 +26,8 @@ class handler(BaseHTTPRequestHandler):
 		dic = dict(parse.parse_qsl(parse.urlsplit(s).query))
 		word = dic["word"]
 		answer = dic['answer']
-		answer_vector = model[answer]
-		word_vector = model[word]
-
-		# Reshape vectors for cosine similarity calculation
-		answer_vector = answer_vector.reshape(1, -1)  # Reshape to row vector
-		word_vector = word_vector.reshape(1, -1)    # Reshape to row vector
-
-		# Calculate cosine similarity between "pizza" and the other word
-		sim = cosine_similarity(answer_vector, word_vector)[0][0]
-		score =  sim * 100
+		sim = nlp(answer).similarity(nlp(word))
+		score = sim * 100
 		result = json.dumps({"score": score})
 		self.send_response(200)
 		self._set_headers()
