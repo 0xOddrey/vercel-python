b9575b9e134a7ceb711a3f44bb3a264f1b9ff38c
0xOddrey <97917541+0xOddrey@users.noreply.github.com>
2024-06-01 11:16:28 -0500

    update

--- api/words/index.py
+++ api/words/index.py
@@ -1,7 +1,8 @@
 from http.server import BaseHTTPRequestHandler
 from urllib import parse
 import json
-
+import gensim.downloader as api
+model = api.load("glove-twitter-25")
 
 class handler(BaseHTTPRequestHandler):
 
@@ -19,6 +20,11 @@ class handler(BaseHTTPRequestHandler):
 
 
 	def do_GET(self):
+		s = self.path
+		dic = dict(parse.parse_qsl(parse.urlsplit(s).query))
+		word = dic["word"]
+		answer = dic['answer']
+		word_vector = model[word]
 		result = json.dumps({"score": 5})
 		self.send_response(200)
 		self._set_headers()
