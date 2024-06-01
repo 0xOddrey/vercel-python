879072bd7c1fb9c1fe94d892108a94e713005b50
0xOddrey <97917541+0xOddrey@users.noreply.github.com>
2024-06-01 13:08:43 -0500

    updates

--- api/words/index.py
+++ api/words/index.py
@@ -1,10 +1,17 @@
 from http.server import BaseHTTPRequestHandler
 from urllib import parse
 import json
-import gensim.downloader as api
+import gensim
 import numpy as np
+import gzip
+import shutil
 
 
+def cosine_similarity(vec1, vec2):
+    dot_product = np.dot(vec1, vec2)
+    norm_vec1 = np.linalg.norm(vec1)
+    norm_vec2 = np.linalg.norm(vec2)
+    return dot_product / (norm_vec1 * norm_vec2)
 
 class handler(BaseHTTPRequestHandler):
 
@@ -22,9 +29,25 @@ class handler(BaseHTTPRequestHandler):
 
 
 	def do_GET(self):
+		compressed_model_path = './glove-wiki-gigaword-50.gz'
+		# Path to the decompressed model file
+		decompressed_model_path = './glove-wiki-gigaword-50'
+
+		# Decompress the model filessss
+		with gzip.open(compressed_model_path, 'rb') as f_in:
+			with open(decompressed_model_path, 'wb') as f_out:
+				shutil.copyfileobj(f_in, f_out)
+
+		model = gensim.models.KeyedVectors.load_word2vec_format(decompressed_model_path, binary=False)
 		s = self.path
 		dic = dict(parse.parse_qsl(parse.urlsplit(s).query))
-		result = json.dumps({"score": 2})
+		word = dic["word"]
+		answer = dic["answer"]
+		answer_vector = model[answer]
+		word_vector = model[word]
+		sim = cosine_similarity(answer_vector, word_vector)
+		sim = sim * 100
+		result = json.dumps({"score": sim})
 		self.send_response(200)
 		self._set_headers()
 		self.end_headers()
